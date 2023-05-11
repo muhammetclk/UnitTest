6e44d3473fcbff4d9aa89bd99b6e00e61b04556d
Muhammet Çelik <muhammetclk56@gmail.com>
2023-05-11 18:53:15 +0300

    testcontext ile testimizdeki veriler hakkinda bilgi aldik.

---
+++ Test8/TestContextDemo/TestContextDemo.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestContextDemo.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestContextDemo.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84aa3c48-f9a3-4037-b849-6a4de48662b3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test8/TestContextDemo/TestContextDemo.Test/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestContextDemo.Test
+{
+    //TestContext, testimizdeki veriler hakkinda bilgi almamizi saglar.
+
+
+    [TestClass]
+    public class UnitTest1
+    {
+        //biz set etmicez runtimeda kendisi set oluyor.ismininde TestContext olmasi gerekiyor.
+        public TestContext TestContext { get; set; }
+
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //istedigimiz herhangi bir bilgiyi output ekranina yazar.
+            TestContext.WriteLine("--TestInitialize--");
+            TestContext.WriteLine("Test Durumu:{0}", TestContext.CurrentTestOutcome);//testin durumunu verir.
+            
+            TestContext.Properties["Bilgi"]="Bu bir ekstra bilgidir.";//(key=bilgi)bu sekilde testimize atabiliriz.
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            //istedigimiz herhangi bir bilgiyi output ekranina yazar.
+            TestContext.WriteLine("--TestCleanup--");            
+            TestContext.WriteLine("Test Durumu:{0}", TestContext.CurrentTestOutcome);//testin durumunu verir.
+            //testten gecip gecmedigini son olarak Cleanup da ogreniriz(output ekranindan)
+
+            TestContext.WriteLine("Test Bilgi:{0}", TestContext.Properties["Bilgi"]);
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //istedigimiz herhangi bir bilgiyi output ekranina yazar.
+            TestContext.WriteLine("--TestMethod1--");
+            TestContext.WriteLine("Test Adı:{0}",TestContext.TestName);//testin adini verir.
+            TestContext.WriteLine("Test Durumu:{0}", TestContext.CurrentTestOutcome);//testin durumunu verir.
+            TestContext.WriteLine("Test class adi:{0}", TestContext.FullyQualifiedTestClassName);
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
